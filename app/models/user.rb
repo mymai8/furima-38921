63a2dc0a5ee470dc1b5527b72826fb4169ec3c2a
mymai8 <mywork1868@gmail.com>
2023-03-02 15:13:47 +0900

    バリデーションの記述の直し2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: "is invalid. Include both letters and numbers"}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters"} do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: "is invalid. Input full-width katakana characters"} do
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
+    validates :birthday, presence: true
 
 end
