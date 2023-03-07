498b31e3241796b7263443dc17828c6c039697cc
mymai8 <mywork1868@gmail.com>
2023-03-07 20:03:54 +0900

    テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_fee_payment
@@ -8,9 +10,10 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
+
   with_options presence: true do
     validates :image
-    validates :user_id
     validates :name
     validates :description
     validates :category_id
@@ -18,8 +21,10 @@ class Item < ApplicationRecord
     validates :delivery_fee_payment_id
     validates :prefecture_id
     validates :shipping_day_id
-    validates :price, inclusion: { in: 300..9_999_999, message: "is out of setting range" }, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
+    validates :price, inclusion: { in: 300..9_999_999, message: "is out of setting range" }
   end
+
+
   
   with_options numericality: {other_than: 1, message: "can't be blank"} do
     validates :category_id
