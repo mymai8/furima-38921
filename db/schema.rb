a2d68b5172dda50aa16037dd6c153a832aa48f40
mymai8 <mywork1868@gmail.com>
2023-03-08 16:29:52 +0900

    テーブルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -33,21 +33,6 @@ ActiveRecord::Schema.define(version: 2023_03_03_082703) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "category_id", null: false
-    t.integer "condition_id", null: false
-    t.integer "delivery_fee_payment_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "shipping_day_id", null: false
-    t.integer "price", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name", null: false
@@ -82,6 +67,5 @@ ActiveRecord::Schema.define(version: 2023_03_03_082703) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "articles", "users"
   add_foreign_key "items", "users"
 end
