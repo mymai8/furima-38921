8a16b63deb169178437ae882259b7e8086305f11
mymai8 <mywork1868@gmail.com>
2023-03-14 21:24:14 +0900

    修正1

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :order_form do
-    user_id { Faker::Number.non_zero_digit }
-    item_id { Faker::Number.non_zero_digit }
     post_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
-    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
     city { Faker::Address.city }
     block { Faker::Address.street_address }
     building_name { Faker::Address.street_address }
--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
   before do
-    @order_form = FactoryBot.build(:order_form)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_form = FactoryBot.build(:order_form, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do
@@ -38,8 +40,8 @@ RSpec.describe OrderForm, type: :model do
         @order_form.building_name = nil
         expect(@order_form).to be_valid
       end
-      it '電話番号が11番桁以内かつハイフンなしであれば保存できる' do
-        @order_form.telephone_number = 12_345_678_910
+      it '電話番号が10または11桁かつハイフンなしであれば保存できる' do
+        @order_form.telephone_number = 12345678901
         expect(@order_form).to be_valid
       end
     end
@@ -95,8 +97,13 @@ RSpec.describe OrderForm, type: :model do
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Telephone number is invalid')
       end
+      it '電話番号が10桁未満だと保存できないこと' do
+        @order_form.telephone_number = 123456789
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Telephone number is invalid')
+      end
       it '電話番号が12桁以上あると保存できないこと' do
-        @order_form.telephone_number = 12_345_678_910_123_111
+        @order_form.telephone_number = 1234567890123
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Telephone number is invalid')
       end
